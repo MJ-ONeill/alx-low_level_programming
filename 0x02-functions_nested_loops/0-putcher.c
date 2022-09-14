d6abfcab94ba723bf948df163ed914d3789fc489
MJ O'Neill <91608528+MJ-ONeill@users.noreply.github.com>
2022-09-14 02:19:29 +0200

    commit

--- 0x02-functions_nested_loops/0-putcher.c
+++ 0x02-functions_nested_loops/0-putcher.c
@@ -1,21 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  print 0, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H');
-	_putchar('o');
-	_putchar('l');
-	_putchar('b');
-	_putchar('e');
-	_putchar('r');
-	_putchar('t');
-	_putchar('o');
-	_putchar('n');
-	_putchar('\n');
+_putchar('\n');
 
 	return (0);
 }
