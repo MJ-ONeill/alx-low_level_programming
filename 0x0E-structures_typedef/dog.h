3c3589f2f5824fcac34e142c3cf5f3d201714c32
MJ O'Neill <91608528+MJ-ONeill@users.noreply.github.com>
2022-10-12 20:12:49 +0200

    initial re commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,24 +1,24 @@
-fndef DOG_H
+#ifndef DOG_H
 #define DOG_H
 
 /**
- *  * struct dog - a dog's basic info
- *   * @name: First member
- *    * @age: Second member
- *     * @owner: Third member
- *      *
- *       * Description: Longer description
- *        */
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
-		char *name;
-			float age;
-				char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
 /**
- *  * dog_t - typedef for struct dog
- *   */
+ * dog_t - typedef for struct dog
+ */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -29,4 +29,3 @@ char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
-
