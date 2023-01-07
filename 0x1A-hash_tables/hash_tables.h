720b14b642b66fb566053e9bd19b8a070d4875ac
MJ O'Neill <91608528+MJ-ONeill@users.noreply.github.com>
2023-01-07 02:17:30 +0200

    commit

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -9,9 +9,9 @@
  * struct hash_node_s - Node of a hash table
  *
  * @key: The key, string
- * The key is unoque in the HashTable
+ * The key is unique in the HashTable
  * @value: The value corresponding to a key
- * @next: Apointer to the next node of the List
+ * @next: A pointer to the next node of the List
  */
 typedef struct hash_node_s
 {
@@ -26,7 +26,7 @@ typedef struct hash_node_s
  * @size: The size of the array
  * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
- * because we want HashTable to use a Chaining collision handling
+ * because we want our HashTable to use a Chaining collision handling
  */
 typedef struct hash_table_s
 {
@@ -35,9 +35,9 @@ typedef struct hash_table_s
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int  hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, condt char *value);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
@@ -62,12 +62,12 @@ typedef struct shash_node_s
 } shash_node_t;
 
 /**
- * struct shash_table_s = sorted hash table data structure
+ * struct shash_table_s - Sorted hash table data structure
  *
  * @size: The size of the array
- * @array: An array of the size @size
+ * @array: An array of size @size
  * Each cell of this array is a pointer to the first node of a linked list,
- * because we want out HashTable to use a chaining collision handling
+ * because we want our HashTable to use a Chaining collision handling
  * @shead: A pointer to the first element of the sorted linked list
  * @stail: A pointer to the last element of the sorted linked list
  */
@@ -79,11 +79,11 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
-shash_table_t *shash_table_create(unned long int size);
+shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-char *shash_table_get(const shahs_table_t *ht, const char *key);
-void shash_table_print(const shash_table_t *het);
-void shash_table_prpint_rev(const shash_table_t *ht);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
 void shash_table_delete(shash_table_t *ht);
 
 #endif /* _HASH_TABLES_H_ */
