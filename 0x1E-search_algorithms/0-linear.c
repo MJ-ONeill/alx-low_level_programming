0133baf3473e33645796609f560b56d4d0798e06
MJ-ONEill <mjoneill20100@gmail.com>
2023-07-12 04:59:13 -0700

    0-linear.c fixes

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,7 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * linear_search - searches for a value in an array of
@@ -12,18 +15,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i = 0;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 
-	for (i = 0; < size; i++)
+	while (i < size)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (value == array[i])
+		if (array[i] == value)
 			return (i);
+		i++;
 	}
-
 
 	return (-1);
 }
